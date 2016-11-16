4c2059681a9795a2a0af2d9a74634f886c7fba8c
rachelgoodman <rachel.goodman@utexas.edu>
2016-11-15 18:59:05 -0600

    step 6
    highlight and unhighlight function

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -2,3 +2,8 @@ function highlight() {
   $('h1').text('Hello!');
   $('.jumbotron').addClass('highlighted');
 }
+
+function unhighlight() {
+  $('h1').text('Hello World!');
+  $('.jumbotron').removeClass('highlighted');
+}
